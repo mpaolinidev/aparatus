4671f7c4a10dcb571d43c00f4f3f7d9c392d5ea9
mpaolinidev <mvspaolini@gmail.com>
2025-11-14 18:46:33 -0300

    feat: add page components

---
+++ app/_components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="p-7">
+      <p className="text-muted-foreground text-xs font-semibold">
+        &copy; 2024 Aparatus.
+      </p>
+      <p className="text-muted-foreground text-xs">
+        Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
